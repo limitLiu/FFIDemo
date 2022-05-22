deaa75efaef40f00a340cf98b52416b4a4b4e774
limit <limitliu@qq.com>
2022-05-22 17:38:29 +0800

    feat:   init

---
+++ app/src/main/cpp/include/llmd5.h
@@ -0,0 +1,18 @@
+//
+// Created by limit on 2022/5/22.
+//
+
+#ifndef FFIDEMO_LLMD5_H
+#define FFIDEMO_LLMD5_H
+
+#if __cplusplus
+extern "C" {
+#endif
+
+const char *ll_md5(const char *buf);
+
+#if __cplusplus
+}
+#endif
+
+#endif //FFIDEMO_LLMD5_H
---
+++ app/src/main/java/wiki/mdzz/ffidemo/MainActivity.kt
@@ -0,0 +1,34 @@
+package wiki.mdzz.ffidemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import wiki.mdzz.ffidemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+  private lateinit var binding: ActivityMainBinding
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    // Example of a call to a native method
+    binding.sampleText.text = stringFromJNI("foo")
+  }
+
+  /**
+   * A native method that is implemented by the 'ffidemo' native library,
+   * which is packaged with this application.
+   */
+  private external fun stringFromJNI(buf: String): String
+
+  companion object {
+    // Used to load the 'ffidemo' library on application startup.
+    init {
+      System.loadLibrary("ffidemo")
+    }
+  }
+}
